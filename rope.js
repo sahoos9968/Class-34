20b5108ef1b0a261f4aa53354d62b5842e10c6da
sahoos9968 <67476477+sahoos9968@users.noreply.github.com>
2021-04-28 17:35:29 -0500

    Add files via upload

---
+++ rope.js
@@ -0,0 +1,28 @@
+class Rope {
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA:bodyA,
+            pointB:pointB,
+            stiffness: 1.2,
+            length: 250
+        }
+
+        this.pointB = pointB;
+        this.rope = Constraint.create(options);
+
+        World.add(world, this.rope);
+    }
+
+    display(){
+        if (this.rope.bodyA){
+            var pointA = this.rope.bodyA.position;
+            var pointB = this.pointB;
+            
+            push();
+            stroke("orange");
+            strokeWeight(3);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            pop();
+        }
+    }
+}
